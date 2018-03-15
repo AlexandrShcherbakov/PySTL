de74130ca08becc4b4dfb732f1d5f19f77ec23bf
AlexandrShcherbakov <alex.shcherbakov.as@gmail.com>
2018-03-15 20:25:55 +0300

    Add tests for enumerate.

--- test/test_enumerate.cpp
+++ test/test_enumerate.cpp
@@ -16,3 +16,26 @@ TEST(VectorEnumerate__Test, EnumerateFunction) {
     enumObject = PySTL::enumerate(v, 1);
     ASSERT_EQ(std::get<0>(enumObject.back()), PySTL::len(v));
 }
+
+
+TEST(ArrayEnumerate__Test, EnumerateFunction) {
+    std::array<float, 3> v = {1, 2, 3};
+    auto enumObject = PySTL::enumerate(v);
+    ASSERT_EQ(PySTL::len(enumObject), PySTL::len(v));
+    for (auto it : enumObject) {
+        ASSERT_EQ(std::get<1>(it), v[std::get<0>(it)]);
+    }
+    enumObject = PySTL::enumerate(v, 1);
+    ASSERT_EQ(std::get<0>(enumObject.back()), PySTL::len(v));
+}
+
+TEST(StringEnumerate__Test, EnumerateFunction) {
+    std::string v = "Test string";
+    auto enumObject = PySTL::enumerate(v);
+    ASSERT_EQ(PySTL::len(enumObject), PySTL::len(v));
+    for (auto it : enumObject) {
+        ASSERT_EQ(std::get<1>(it), v[std::get<0>(it)]);
+    }
+    enumObject = PySTL::enumerate(v, 1);
+    ASSERT_EQ(std::get<0>(enumObject.back()), PySTL::len(v));
+}
