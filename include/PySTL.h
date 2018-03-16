67b514a85f8233d353709f87e4572ad86fdaf823
AlexandrShcherbakov <alex.shcherbakov.as@gmail.com>
2018-03-16 21:25:14 +0300

    Add range function.

--- include/PySTL.h
+++ include/PySTL.h
@@ -58,6 +58,54 @@ namespace PySTL {
         }
 
     };
+
+    class range {
+        const int Begin, End, Step;
+
+        class RangeIterator {
+            int Idx;
+            const int Begin, End, Step;
+        public:
+            RangeIterator(const int idx, const int begin, const int end, const int step):
+                Idx(idx), Begin(begin), End(end), Step(step) {}
+
+            int operator*() {
+                return Idx;
+            }
+
+            void operator++() {
+                Idx += Step;
+            }
+
+            bool operator==(const RangeIterator & it) const {
+                /// All iterators out of range are equal
+                const bool firstOutOfRange = Step * (End - Idx) <= 0;
+                const bool secondOutOfRange = it.Step * (it.End - it.Idx) <= 0;
+                return (Idx == it.Idx || firstOutOfRange && secondOutOfRange)
+                    && Step == it.Step
+                    && Begin == it.Begin
+                    && End == it.End;
+            }
+
+            bool operator!=(const RangeIterator & it) const {
+                return !(*this == it);
+            }
+        };
+    public:
+        using value_type = int;
+        range(const int begin, const int end, const int step=1): Begin(begin), End(end), Step(step) {
+            assert(Step);
+        }
+        range(const int end) : range(0, end) {}
+
+        RangeIterator begin() {
+            return RangeIterator(Begin, Begin, End, Step);
+        }
+
+        RangeIterator end() {
+            return RangeIterator(End, Begin, End, Step);
+        }
+    };
 }
 
 #endif //PYSTL_MAIN_H
---
+++ test/test_range.cpp
@@ -0,0 +1,42 @@
+//
+// Created by alex on 16.03.18.
+//
+
+#include "gtest/gtest.h"
+
+#include "../include/PySTL.h"
+
+using namespace PySTL;
+
+TEST(RangeToEnd__Test, RangeFunction) {
+    for (auto x: range(10)) {
+        ASSERT_LT(x, 10);
+        ASSERT_GE(x, 0);
+    }
+}
+
+TEST(RangeBeginToEnd__Test, RangeFunction) {
+    for (auto x: range(10, 20)) {
+        ASSERT_LT(x, 20);
+        ASSERT_GE(x, 10);
+    }
+}
+
+TEST(RangeBeginToEndWithStep__Test, RangeFunction) {
+    int count = 0;
+    for (auto x: range(10, 20, 2)) {
+        ASSERT_LT(x, 20);
+        ASSERT_GE(x, 10);
+        count++;
+    }
+    ASSERT_EQ(count, 5);
+
+    count = 0;
+    for (auto x: range(10, 20, 3)) {
+        ASSERT_LT(x, 20);
+        ASSERT_GE(x, 10);
+        ASSERT_TRUE(x % 3 == 1);
+        count++;
+    }
+    ASSERT_EQ(count, 4);
+}
