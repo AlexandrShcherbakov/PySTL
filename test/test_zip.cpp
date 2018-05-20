f8595468748c9c9add2d108d74a5554e9af3e569
AlexandrShcherbakov <alex.shcherbakov.as@gmail.com>
2018-05-20 23:58:24 +0300

    Add base implementation of zip function.

---
+++ test/test_zip.cpp
@@ -0,0 +1,23 @@
+//
+// Created by alex on 20.05.2018.
+//
+
+#include "gtest/gtest.h"
+
+#include "../include/PySTL.h"
+
+using namespace PySTL;
+
+TEST(OneArgZip__Test, ZipFunction) {
+    std::vector<int> a(10, 5);
+    for (const auto [i] : zip(a)) {
+        ASSERT_EQ(i, 5);
+    }
+}
+
+TEST(TwoArgZip__Test, ZipFunction) {
+    std::vector<int> a = {1, 2, 3, 4, 5, 6, 7};
+    for (const auto [i, j] : zip(a, a)) {
+        ASSERT_EQ(i, j);
+    }
+}
