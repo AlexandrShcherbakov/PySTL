f7b5cde4e07716934d961c4edd4b64c787f66e07
Denis Shvetsov <dshvetsov@arccn.ru>
2018-05-24 20:12:57 +0300

    Use of class to holder print

--- include/Print.h
+++ include/Print.h
@@ -8,17 +8,72 @@ namespace PySTL
 // TODO [dshvetsov] : Add support of output specification
 //                    (such set_precision, set_base, etc).
 
-template <typename Head>
-void print(Head&& h)
-{
-    std::cout << std::forward<Head>(h) << std::endl;
-}
+namespace detail {
 
 template <typename Head, typename... Args>
-void print(Head&& h, Args&&... args)
+class __print_impl
 {
-    std::cout << std::forward<Head>(h) << " " ;
-    print(std::forward<Args>(args)...)...;
+public:
+    __print_impl(const Head& head, const Args&... tail)
+        : m_head(head), m_tail(tail...)
+    { }
+
+    void flush(std::ostream& out)
+    {
+        out << m_head << " ";
+        m_tail.flush(out);
+    }
+
+private:
+    const Head& m_head;
+    __print_impl<Args...> m_tail;
+};
+
+template <typename Head>
+class __print_impl<Head>
+{
+public:
+    __print_impl(const Head& head)
+        : m_head(head)
+    { }
+
+    void flush(std::ostream& out) {
+        out << m_head;
+    }
+private:
+    const Head& m_head;
+};
+
+template <typename... Args>
+class __print_holder {
+public:
+    __print_holder(const Args&... args)
+        : m_data(args...)
+    { }
+
+    ~__print_holder() noexcept
+    {
+        m_data.flush(m_out);
+    }
+
+    // Cannot be copied or moved
+    __print_holder(const __print_holder&) = delete;
+    __print_holder(__print_holder&&) = delete;
+
+private:
+    __print_impl<Args...> m_data;
+    std::ostream& m_out = std::cout;
+//    std::string delim = " ";
+//    std::string endline = "\n";
+};
+
+
+} // namespace deatail
+
+template <typename... Args>
+detail::__print_holder<Args...> print(const Args&... args)
+{
+    return detail::__print_holder<Args...>(args...);
 }
 
 } // namespace PySTL
