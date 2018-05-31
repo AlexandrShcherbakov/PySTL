04985c4f6aa4bac71cc1d32a205f804fd4e90c89
Denis Shvetsov <dshvetsov@arccn.ru>
2018-05-31 13:18:35 +0300

    remove TODO list

--- include/Print.h
+++ include/Print.h
@@ -4,11 +4,6 @@
 namespace PySTL
 {
 
-// TODO [dshvetsov] : Make more efficient.
-// TODO [dshvetsov] : Add support to write in any ostream.
-// TODO [dshvetsov] : Add support of output specification
-//                    (such set_precision, set_base, etc).
-
 namespace detail {
 
 template <typename Head, typename... Args>
